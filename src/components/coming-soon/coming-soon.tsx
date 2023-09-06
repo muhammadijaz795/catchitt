b4691560620699fb6fb4038c858add89d35a2c30
Islam Ibrahim <islamibrahim.email@gmail.com>
2023-09-06 21:30:16 +0300

    Coming soon page nav

--- src/components/coming-soon/coming-soon.tsx
+++ src/components/coming-soon/coming-soon.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
-// import classNames from 'classnames';
+import classNames from 'classnames';
 import styles from './coming-soon.module.scss';
+import { TopBar } from '../top-bar/top-bar';
+import { SideNavBar } from '../side-nav-bar/side-nav-bar';
+import { SuggestedActivity } from '../suggested-activity/suggested-activity';
 
 export interface ComingSoonProps {
     className?: string;
 }
 
-const ComingSoon: React.FC = ({ }: ComingSoonProps) => {
+const ComingSoon: React.FC = ({ className }: ComingSoonProps) => {
     return (
-        <div className={styles.container}>
-            <h1>Coming Soon</h1>
+        <div className={classNames(styles.root, className)}>
+            <div className={styles.topBarDiv}>
+                <TopBar />
+            </div>
+            <div className={styles.container}>
+                <div className={styles.leftSide}>
+                    <div className={styles.sideNavDiv}>
+                        <SideNavBar selectedIndex={1} />
+                    </div>
+                    <div className={styles.suggestedActivityDiv}>
+                        <SuggestedActivity showActivity={true} showSuggestedContent={true} />
+                    </div>
+                </div>
+                <div className={styles.middleSectionDiv}>
+                    <div className={styles.suggestedContent}>
+                        <h4 style={{ alignSelf: 'center' }}>Coming Soon</h4>
+                    </div>
+                </div>
+            </div>
         </div>
+
     );
 };
 
