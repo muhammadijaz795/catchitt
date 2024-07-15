09ef3687883e7258a92bbbee80374e333b91fd6f
EhsanSoftwareEngineer <146177587+EhsanSoftwareEngineer@users.noreply.github.com>
2024-07-16 04:56:40 +0500

    no message

--- src/components/profile/svg-components/IconlyLightEditSquare.tsx
+++ src/components/profile/svg-components/IconlyLightEditSquare.tsx
@@ -4,9 +4,9 @@ function IconlyLightEditSquare() {
     return (
         <svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="16" cy="16.5" r="16" fill="#EEEDF7" />
-            <path d="M15.5741 8.82422H12.4582C9.89573 8.82422 8.28906 10.6384 8.28906 13.2067V20.1351C8.28906 22.7034 9.88823 24.5176 12.4582 24.5176H19.8116C22.3824 24.5176 23.9816 22.7034 23.9816 20.1351V16.7784" stroke="#5448B2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M13.3575 15.6007L19.585 9.37324C20.3608 8.59824 21.6183 8.59824 22.3942 9.37324L23.4083 10.3874C24.1842 11.1632 24.1842 12.4216 23.4083 13.1966L17.1508 19.4541C16.8117 19.7932 16.3517 19.9841 15.8717 19.9841H12.75L12.8283 16.8341C12.84 16.3707 13.0292 15.9291 13.3575 15.6007Z" stroke="#5448B2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M18.6328 10.335L22.4378 14.14" stroke="#5448B2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M15.5741 8.82422H12.4582C9.89573 8.82422 8.28906 10.6384 8.28906 13.2067V20.1351C8.28906 22.7034 9.88823 24.5176 12.4582 24.5176H19.8116C22.3824 24.5176 23.9816 22.7034 23.9816 20.1351V16.7784" stroke="rgb(255, 59, 92)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M13.3575 15.6007L19.585 9.37324C20.3608 8.59824 21.6183 8.59824 22.3942 9.37324L23.4083 10.3874C24.1842 11.1632 24.1842 12.4216 23.4083 13.1966L17.1508 19.4541C16.8117 19.7932 16.3517 19.9841 15.8717 19.9841H12.75L12.8283 16.8341C12.84 16.3707 13.0292 15.9291 13.3575 15.6007Z" stroke="rgb(255, 59, 92)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M18.6328 10.335L22.4378 14.14" stroke="rgb(255, 59, 92)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
         </svg>
 
     )
