816be14b3d2b9171aba1b5866dde638099b54180
Omar Indalib <mindalib@ogoul.com>
2024-08-22 19:39:55 +0500

    Time Duration

--- src/shared/navbar/components/Notifications.tsx
+++ src/shared/navbar/components/Notifications.tsx
@@ -164,7 +164,7 @@ export default function NavbarMunu({ onViewProfile, Onlogout, onSettings }: any)
                         </div>   
                     </div>
                     <div className={`${style.inboxList}`}>
-                        <p>Today</p>
+                        <p className={`${style.inboxListDuration}`}>Today</p>
                         <div className={`${style.notificationList}`}>
                             <div className={`${style.inboxListItem}`}>
                                 <div className={`${style.inboxListInner}`}>
@@ -179,7 +179,7 @@ export default function NavbarMunu({ onViewProfile, Onlogout, onSettings }: any)
                                 <button className={`${style.inboxFollow}`}>Follow back</button>
                             </div>
                         </div>
-                        <p>Yesterday</p>
+                        <p className={`${style.inboxListDuration}`}>Yesterday</p>
                         <div className={`${style.notificationList}`}>
                             <div className={`${style.inboxListItem}`}>
 
@@ -195,7 +195,7 @@ export default function NavbarMunu({ onViewProfile, Onlogout, onSettings }: any)
                                 <button className={`${style.inboxFriends}`}><svg width="1em" data-e2e="" height="1em" viewBox="0 0 48 48" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M43.4143 16.5858L31.1214 4.29289C30.7309 3.90237 30.0977 3.90237 29.7072 4.29289L28.2929 5.70711C27.9024 6.09763 27.9024 6.7308 28.293 7.12132L37.1716 16H5.00006C4.44777 16 4.00006 16.4477 4.00006 17V19C4.00006 19.5523 4.44777 20 5.00006 20H42.0001C42.809 20 43.5383 19.5127 43.8478 18.7654C44.1574 18.018 43.9863 17.1578 43.4143 16.5858Z"></path><path d="M4.58584 31.4142L16.8787 43.7071C17.2693 44.0976 17.9024 44.0976 18.293 43.7071L19.7072 42.2929C20.0977 41.9024 20.0977 41.2692 19.7072 40.8787L10.8285 32L43.0001 32C43.5523 32 44.0001 31.5523 44.0001 31V29C44.0001 28.4477 43.5523 28 43.0001 28L6.00006 28C5.19113 28 4.46186 28.4873 4.1523 29.2346C3.84274 29.982 4.01385 30.8422 4.58584 31.4142Z"></path></svg> Friends</button>
                             </div>
                         </div>
-                        <p>This week</p>
+                        <p className={`${style.inboxListDuration}`}>This week</p>
                         <div className={`${style.notificationList}`}>
                             <div className={`${style.inboxListItem}`}>
                                 <div className={`${style.inboxListInner}`}>
@@ -212,7 +212,7 @@ export default function NavbarMunu({ onViewProfile, Onlogout, onSettings }: any)
                                 </div>
                             </div>
                         </div>
-                        <p>This month</p>
+                        <p className={`${style.inboxListDuration}`}>This month</p>
                         <div className={`${style.notificationList}`}>
                             <div className={`${style.inboxListItem}`}>
                                 <div className={`${style.inboxListInner}`}>
@@ -230,7 +230,7 @@ export default function NavbarMunu({ onViewProfile, Onlogout, onSettings }: any)
                                 </div>
                             </div>
                         </div>
-                        <p>Previous</p>
+                        <p className={`${style.inboxListDuration}`}>Previous</p>
                         <div className={`${style.notificationList}`}>
                             <div className={`${style.inboxListItem}`}>
                                 <div className={`${style.inboxListInner}`}>
