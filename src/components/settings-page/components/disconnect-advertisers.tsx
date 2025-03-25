6b09d66a61dce95219e40a7a78fa7440805b6df9
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2025-03-25 14:34:39 +0500

    Update disconnect-advertisers.tsx

--- src/components/settings-page/components/disconnect-advertisers.tsx
+++ src/components/settings-page/components/disconnect-advertisers.tsx
@@ -1,16 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Typography } from "@mui/material";
 import { styled } from "@mui/material";
 
 
 const DisconnectAdvertisers: React.FC = () => {
- 
+    const [Sponsored, setSponsored] = useState(
+        {
+            items: [],
+            isLoading: false,
+            canLoadMore: false,
+        }
+    );
+     
+    function fetchCategories()
+    { 
+        setSponsored(prev => ({ ...prev, isLoading: true }));
+        let endpoint = process.env.VITE_API_URL + '/profile/v2/disconnect-advertiser';
+        let payload =
+        {
+            method: 'GET',
+            headers:
+            {
+                'Content-Type': 'application/json',
+                'Authorization': "Bearer " + localStorage.getItem('token'),
+            },
+        };
+
+        fetch(endpoint, payload)
+        .then(response => response.ok ? response.json() : Promise.reject("Failed to fetch data"))
+        .then(data => setSponsored({ items: data.data, isLoading: false, canLoadMore: data.data.length > 0 }))
+        .catch(error => setSponsored(prev => ({ ...prev, isLoading: false })));
+    }
+
+    useEffect(fetchCategories, []);
  
 
  
   return (
       <div className=" w-100 p-3">
-        <div className="d-flex justify-between border-top border-bottom py-3" >
+        <div className="d-flex justify-between border-top border-bottom py-3 d-none" >
                 <div className='text-left'>
                     <span className=' text-left text-sm text-[#16182399]'>Advertisers you have disconnected</span>
                 </div>
@@ -19,7 +47,7 @@ const DisconnectAdvertisers: React.FC = () => {
                 </svg>
         </div>
         <Typography className="border-top pt-3">
-            <div className="d-flex justify-between">
+            <div className="d-flex justify-between py-3 d-none">
                 <div >
                     <div className='text-left'>
                         <p className='text-base'>Smart Valley</p>
