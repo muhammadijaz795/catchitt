bc332a6b603bb8f48fa47a079961635341fc43d7
Muhammad Asif <masif@ogoul.com>
2025-04-22 17:02:43 +0500

    detail overview post

--- src/components/analytics/PostAnalytics/index.tsx
+++ src/components/analytics/PostAnalytics/index.tsx
@@ -8,6 +8,29 @@ import OverviewTab from './tabs/OverviewTab';
 import ViewersTab from './tabs/ViewersTab';
 import EngagementTab from './tabs/EngagementTab';
 import { isValidDocId } from '../../../utils/helpers';
+import {  logo, logoAuth, logoAuthWhite } from '../../../icons';
+import {
+    AppBar,
+    Avatar,
+    Box,
+    Card,
+    Divider,
+    Grid,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemText,
+    Tab,
+    Tabs,
+    Toolbar,
+    Typography,
+  } from '@mui/material';
+  import { ExpandMore } from '@mui/icons-material';
+  
+  const thumbnails = Array.from({ length: 12 }).map((_, index) => ({
+    image: 'https://via.placeholder.com/60', // Replace with your thumbnail images
+    description: 'No description',
+  }));
 
 const PostAnalytics = () => {
     const [currentTab, setCurrentTab] = useState(POSTANALYTICSTABS.OVERVIEW);
@@ -66,24 +89,32 @@ const PostAnalytics = () => {
         getPostAnalytics();
         fetchPost();
     }, [postId]);
+    const [logo, setLogo] = useState(logoAuth);
 
     const [darkTheme, setdarkTheme] = useState<any>('');
 
     useEffect(() => {
         var themeColor = window.localStorage.getItem('theme');
-        if (themeColor == 'dark') {
-            setdarkTheme(styles.darkTheme);
-        }
+        
+         if (themeColor == "dark") {
+                    setdarkTheme(styles.darkTheme);
+                    setLogo(logoAuthWhite);
+                } else {
+                    setLogo(logoAuth);
+            }
     });
 
     return (
-        <Layout>
-            <header className={`text-gray-600 body-font ${darkTheme===''?'bg-white':'bg-[#181818]'} border-b px-4`}>
-                <div className="flex flex-wrap flex-col md:flex-row items-center justify-between">
+        <>
+            <header className={`text-gray-600 d-flex body-font ${darkTheme===''?'bg-white':'bg-[#181818]'} border-b `}>
+                <div onClick={() => navigate('/')} className={`${styles.sec1} border-r w-[14rem] pl-3`}>
+                    <img src={logo} alt="" />
+                </div>
+                <div className="flex flex-wrap flex-col md:flex-row items-center justify-between w-[calc(100%-14rem)] ml-auto px-4">
                     <nav className="flex flex-wrap items-center text-base text-gray-400">
-                        <a onClick={switchTab} className={`${currentTab===POSTANALYTICSTABS.OVERVIEW?'text-gray-500 font-semibold border-b border-gray-800':''} py-3 mr-5 ${darkTheme===''?'hover:text-gray-900':'hover:text-white'} cursor-pointer`} id={POSTANALYTICSTABS.OVERVIEW.toString()}>Overview</a>
-                        {/* <a onClick={switchTab} className={`${currentTab===POSTANALYTICSTABS.VIEWERS?'text-gray-500 font-semibold border-b border-gray-800':''} py-3 mr-5 ${darkTheme===''?'hover:text-gray-900':'hover:text-white'} cursor-pointer`} id={POSTANALYTICSTABS.VIEWERS.toString()}>Viewers</a> */}
-                        {/* <a onClick={switchTab} className={`${currentTab===POSTANALYTICSTABS.ENGAGEMENT?'text-gray-500 font-semibold border-b border-gray-800':''} py-3 ${darkTheme===''?'hover:text-gray-900':'hover:text-white'} cursor-pointer`} id={POSTANALYTICSTABS.ENGAGEMENT.toString()}>Engagement</a> */}
+                        <a onClick={switchTab} className={`${currentTab===POSTANALYTICSTABS.OVERVIEW?'text-black font-semibold border-b-2 border-black':''} py-3 mr-5 ${darkTheme===''?'hover:text-gray-900':'hover:text-white'} cursor-pointer`} id={POSTANALYTICSTABS.OVERVIEW.toString()}>Overview</a>
+                         <a onClick={switchTab} className={`${currentTab===POSTANALYTICSTABS.VIEWERS?'text-black font-semibold border-b-2 border-black':''} py-3 mr-5 ${darkTheme===''?'hover:text-gray-900':'hover:text-white'} cursor-pointer`} id={POSTANALYTICSTABS.VIEWERS.toString()}>Viewers</a> 
+                        <a onClick={switchTab} className={`${currentTab===POSTANALYTICSTABS.ENGAGEMENT?'text-black font-semibold border-b-2 border-black':''} py-3 ${darkTheme===''?'hover:text-gray-900':'hover:text-white'} cursor-pointer`} id={POSTANALYTICSTABS.ENGAGEMENT.toString()}>Engagement</a> 
                     </nav>
                     <div className="inline-flex lg:justify-end ml-5 lg:ml-0 my-2">
                         {/* <button className="inline-flex mx-2 items-center bg-gray-100  border-0 py-2 px-3 focus:outline-none hover:bg-gray-200 rounded-full text-sm">Last 7 Days &#129087;</button> */}
@@ -93,8 +124,38 @@ const PostAnalytics = () => {
                     </div>
                 </div>
             </header>
-
-            {(()=>{
+            <div className='d-flex'>
+                <Box sx={{ width: '14rem', borderRightWidth: '1px' }}>
+                    <Typography variant="body2" fontWeight="bold" sx={{ mb: 2, ml: 1, display: 'flex', borderBottomWidth: '1px', py: 2, px: 1, cursor: 'pointer' }}>
+                        <svg className='mr-2' width="8" height="12" viewBox="0 0 9 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M2.90954 7.44851L8.60121 1.75267C8.67881 1.67461 8.72237 1.569 8.72237 1.45892C8.72237 1.34885 8.67881 1.24324 8.60121 1.16517L7.83454 0.398507C7.75648 0.320903 7.65087 0.277344 7.54079 0.277344C7.43072 0.277344 7.32511 0.320903 7.24704 0.398507L0.492878 7.15684C0.453825 7.19557 0.422827 7.24166 0.401674 7.29243C0.38052 7.34321 0.369629 7.39767 0.369629 7.45267C0.369629 7.50768 0.38052 7.56214 0.401674 7.61291C0.422827 7.66369 0.453825 7.70977 0.492878 7.74851L7.24288 14.5027C7.28161 14.5417 7.3277 14.5727 7.37847 14.5939C7.42925 14.615 7.48371 14.6259 7.53871 14.6259C7.59372 14.6259 7.64818 14.615 7.69895 14.5939C7.74973 14.5727 7.79581 14.5417 7.83454 14.5027L8.60121 13.736C8.67881 13.6579 8.72237 13.5523 8.72237 13.4423C8.72237 13.3322 8.67881 13.2266 8.60121 13.1485L2.90954 7.44851Z" fill="black"/>
+                        </svg>
+                        Back
+                    </Typography>
+                    <List dense>
+                    {thumbnails.map((item, idx) => (
+                        <ListItem key={idx} sx={{ display: 'flex', gap: 1.5, borderRadius: 2, mb: 1, width: '90%', mx: 'auto', cursor: 'pointer','&:hover': {
+      backgroundColor: '#0000000D',
+    }, py: 1 }}>
+                        <ListItemAvatar>
+                            <Avatar
+                            variant="rounded"
+                            src={item.image}
+                            sx={{ width: 45, height: 50, mb: 0.5 }}
+                            />
+                        </ListItemAvatar>
+                        <ListItemText
+                            primary={
+                            <Typography variant="caption" color="text.secondary" textAlign="center">
+                                {item.description}
+                            </Typography>
+                            }
+                        />
+                        </ListItem>
+                    ))}
+                    </List>
+                </Box>
+                {(()=>{
                 switch(currentTab){
                     case POSTANALYTICSTABS.OVERVIEW:
                         return <OverviewTab postAnalytics={postAnalytics} post={postData} isDarkTheme={darkTheme} />    
@@ -106,7 +167,9 @@ const PostAnalytics = () => {
                         return <OverviewTab postAnalytics={postAnalytics} />
                 }
             })()}
-        </Layout>
+            </div>
+            
+        </>
     )
 };
 
--- src/components/analytics/PostAnalytics/tabs/OverviewTab.tsx
+++ src/components/analytics/PostAnalytics/tabs/OverviewTab.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { POSTSTATISTICSTABS } from '../../../../utils/constants';
 import { commentOutline, commentOutlineWhite, filledComment, filledCommentWhite, filledHeart, filledHeartWhite, filledSave, filledSaveWhite, filledShare, filledShareWhite, heartOutline, heartOutlineWhite, play, playOutline, playOutlineWhite } from '../../../../icons';
+import { Box, LinearProgress } from '@mui/material';
 
 // {
 //     "status": 200,
@@ -32,7 +33,7 @@ function OverviewTab({ postAnalytics, post, isDarkTheme }: any) {
     const switchTab = (event: React.MouseEvent<HTMLDivElement>) => {
         setActiveTab(Number(event.currentTarget.id));
     }
-
+    const value = 70;
     const possibleGraphs = ['viewsGraph', 'playtimeGraph', 'watchtimeGraph', 'fullwatchedGraph', 'newfollowersGraph']
 
     const convertDateFormat = (inputDate:string) => {
@@ -83,7 +84,7 @@ function OverviewTab({ postAnalytics, post, isDarkTheme }: any) {
     }, [activeTab])
 
     return (
-        <div className='max-w-5xl mx-auto mt-8  overflow-hidden'>
+        <div className='w-[calc(100%-14rem)] px-3 mt-8  overflow-hidden'>
             {/* post data card  */}
             <div className={`${isDarkTheme?'bg-[#181818]':'bg-white'} rounded shadow-sm flex items-center justify-between p-3 mb-4`}>
                 <div className='flex items-center space-x-4'>
@@ -92,25 +93,25 @@ function OverviewTab({ postAnalytics, post, isDarkTheme }: any) {
                     </div>
                     <div>
                         <span>{post?.description?.length > 30 ? post?.description?.slice(0, 30) + '...' : ''}</span>
-                        <div className="text-gray-400 text-sm text-left ">{formatDate(post?.createdTime)} </div>
+                        <div className="text-gray-400 text-xs text-left ">Posted on {formatDate(post?.createdTime)} </div>
                     </div>
                 </div>
                 <div className='flex gap-12 mr-3'>
-                    <div className="text-gray-400 flex-col"><img className='w-6' src={isDarkTheme ? playOutlineWhite : play} alt="like" /> <span>{postAnalytics?.views || 0}</span></div>
-                    <div className="text-gray-400 flex-col"><img className='w-6' src={isDarkTheme ? filledHeartWhite : filledHeart} alt="like" /> <span>{postAnalytics?.likes || 0}</span></div>
-                    <div className="text-gray-400 flex-col"><img className='w-6' src={isDarkTheme ? filledCommentWhite : filledComment} alt="like" /> <span>{postAnalytics?.comments || 0}</span></div>
-                    <div className="text-gray-400 flex-col"><img className='w-6' src={isDarkTheme ? filledShareWhite : filledShare} alt="like" /> <span>{postAnalytics?.shares || 0}</span></div>
-                    <div className="text-gray-400 flex-col"><img className='w-6' src={isDarkTheme ? filledSaveWhite : filledSave} alt="like" /> <span>{postAnalytics?.saves || 0}</span></div>
+                    <div className="text-gray-400 flex-col text-sm"><img className='w-5 invert' src={isDarkTheme ? playOutlineWhite : play} alt="like" /> <span>{postAnalytics?.views || 0}</span></div>
+                    <div className="text-gray-400 flex-col text-sm"><img className='w-5 invert' src={isDarkTheme ? filledHeartWhite : filledHeart} alt="like" /> <span>{postAnalytics?.likes || 0}</span></div>
+                    <div className="text-gray-400 flex-col text-sm"><img className='w-5 invert' src={isDarkTheme ? filledCommentWhite : filledComment} alt="like" /> <span>{postAnalytics?.comments || 0}</span></div>
+                    <div className="text-gray-400 flex-col text-sm"><img className='w-5 invert' src={isDarkTheme ? filledShareWhite : filledShare} alt="like" /> <span>{postAnalytics?.shares || 0}</span></div>
+                    <div className="text-gray-400 flex-col text-sm"><img className='w-5 invert' src={isDarkTheme ? filledSaveWhite : filledSave} alt="like" /> <span>{postAnalytics?.saves || 0}</span></div>
                 </div>
             </div>
             {/* statistics card */}
             <div className={`${isDarkTheme?'bg-[#181818]':'bg-white'} rounded shadow-sm mb-4`}>
                 <div className='inline-flex w-full ' >
-                    <div onClick={switchTab} id={POSTSTATISTICSTABS.VIDEO_VIEWS.toString()} className={`cursor-pointer w-1/5 py-16 ${activeTab === POSTSTATISTICSTABS.VIDEO_VIEWS ? 'border-t-red-500 border-t-4' : 'border-t border-b rounded-bl-md'} rounded-tl-md`}>Video views <br /><span className='text-2xl font-semibold'>{postAnalytics?.views || 0}</span></div>
-                    <div onClick={switchTab} id={POSTSTATISTICSTABS.TOTAL_PLAY_TIME.toString()} className={`cursor-pointer w-1/5 py-16 ${activeTab === POSTSTATISTICSTABS.TOTAL_PLAY_TIME ? 'border-t-red-500 border-t-4' : 'border-t border-b'} border-x`}>Total play time <br /> <span className='text-2xl font-semibold'>{totalPlayTime}</span></div>
-                    <div onClick={switchTab} id={POSTSTATISTICSTABS.AVERAGE_WATCH_TIME.toString()} className={`cursor-pointer w-1/5 py-16 ${activeTab === POSTSTATISTICSTABS.AVERAGE_WATCH_TIME ? 'border-t-red-500 border-t-4' : 'border-t border-b'} border-x`}>Share Video <br /> <span className='text-2xl font-semibold'>{postAnalytics?.shares || 0}</span></div>
-                    <div onClick={switchTab} id={POSTSTATISTICSTABS.FULL_WATCH_PERCENTAGE.toString()} className={`cursor-pointer w-1/5 py-16 ${activeTab === POSTSTATISTICSTABS.FULL_WATCH_PERCENTAGE ? 'border-t-red-500 border-t-4' : 'border-t border-b'} border-x`}>Watched full video <br /> <span className='text-2xl font-semibold'>{postAnalytics?.watchedFullVideo?.percentage || 100}%</span></div>
-                    <div onClick={switchTab} id={POSTSTATISTICSTABS.NEW_FOLLOWERS.toString()} className={`cursor-pointer w-1/5 py-16 ${activeTab === POSTSTATISTICSTABS.NEW_FOLLOWERS ? 'border-t-red-500 border-t-4' : 'border-t border-b rounded-br-md'} rounded-tr-md`}>New followers <br /> <span className='text-2xl font-semibold'>{postAnalytics?.reachedAudience?.count || 0}</span></div>
+                    <div onClick={switchTab} id={POSTSTATISTICSTABS.VIDEO_VIEWS.toString()} className={`cursor-pointer w-1/5 py-16 font-semibold ${activeTab === POSTSTATISTICSTABS.VIDEO_VIEWS ? 'border-t-blue-500 border-t-4' : 'border-t border-b rounded-bl-md'} rounded-tl-md`}>Video views <br /><span className={`${activeTab === POSTSTATISTICSTABS.VIDEO_VIEWS ? 'text-blue-500' : ''} text-2xl font-semibold`}>{postAnalytics?.views || 0}</span></div>
+                    <div onClick={switchTab} id={POSTSTATISTICSTABS.TOTAL_PLAY_TIME.toString()} className={`cursor-pointer w-1/5 py-16 font-semibold ${activeTab === POSTSTATISTICSTABS.TOTAL_PLAY_TIME ? 'border-t-blue-500 border-t-4' : 'border-t border-b'} border-x`}>Total play time <br /> <span className={`${activeTab === POSTSTATISTICSTABS.TOTAL_PLAY_TIME ? 'text-blue-500' : ''} text-2xl font-semibold`}>{totalPlayTime}</span></div>
+                    <div onClick={switchTab} id={POSTSTATISTICSTABS.AVERAGE_WATCH_TIME.toString()} className={`cursor-pointer w-1/5 py-16 font-semibold ${activeTab === POSTSTATISTICSTABS.AVERAGE_WATCH_TIME ? 'border-t-blue-500 border-t-4' : 'border-t border-b'} border-x`}>Share Video <br /> <span className={`${activeTab === POSTSTATISTICSTABS.AVERAGE_WATCH_TIME ? 'text-blue-500' : ''} text-2xl font-semibold`}>{postAnalytics?.shares || 0}</span></div>
+                    <div onClick={switchTab} id={POSTSTATISTICSTABS.FULL_WATCH_PERCENTAGE.toString()} className={`cursor-pointer w-1/5 py-16 font-semibold ${activeTab === POSTSTATISTICSTABS.FULL_WATCH_PERCENTAGE ? 'border-t-blue-500 border-t-4' : 'border-t border-b'} border-x`}>Watched full video <br /> <span className={`${activeTab === POSTSTATISTICSTABS.FULL_WATCH_PERCENTAGE ? 'text-blue-500' : ''} text-2xl font-semibold`}>{postAnalytics?.watchedFullVideo?.percentage || 100}%</span></div>
+                    <div onClick={switchTab} id={POSTSTATISTICSTABS.NEW_FOLLOWERS.toString()} className={`cursor-pointer w-1/5 py-16 font-semibold ${activeTab === POSTSTATISTICSTABS.NEW_FOLLOWERS ? 'border-t-blue-500 border-t-4' : 'border-t border-b rounded-br-md'} rounded-tr-md`}>New followers <br /> <span className={`${activeTab === POSTSTATISTICSTABS.NEW_FOLLOWERS ? 'text-blue-500' : ''}  text-2xl font-semibold`}>{postAnalytics?.reachedAudience?.count || 0}</span></div>
                 </div>
                 {/* Chart Section */}
                 {/* <div className="m-4 border-t pt-6 mb-6 ">
@@ -156,7 +157,7 @@ function OverviewTab({ postAnalytics, post, isDarkTheme }: any) {
             <div className="grid grid-cols-2 gap-4">
                 <div className={`${isDarkTheme?'bg-[#181818]':'bg-white'} shadow-sm rounded`}>
                     <div className='py-2 px-4 border-b mb-4 text-left'>
-                        <span className={`${isDarkTheme?'text-gray-300':'text-gray-600'} text-sm`}>Retention rate</span>
+                        <span className={`${isDarkTheme?'text-gray-300':'text-black'} text-[15px] font-semibold`}>Retention rate</span>
                     </div>
                     {/* <p className="text-gray-400 text-sm">
                         Most viewers stopped watching at 0:04. play the video below to see when they lost interest.
@@ -165,31 +166,46 @@ function OverviewTab({ postAnalytics, post, isDarkTheme }: any) {
                         <video onLoadedMetadata={getMediaInfo} className='w-44 h-80 m-auto' controls src={post?.reducedVideoUrl?.length > 0? post?.reducedVideoUrl: post?.originalUrl} />
                     </div>
                 </div>
-                <div className={`${isDarkTheme?'bg-[#181818]':'bg-white'} shadow-sm rounded`}>
-                    <div className='py-2 px-3 border-b mb-4 text-left'>
-                        <span className={`${isDarkTheme?'text-gray-300':'text-gray-600'} text-sm mb-2`}>Traffic source</span>
+                <div className={`${isDarkTheme?'bg-[#181818]':'bg-white'} shadow-sm text-left rounded`}>
+                    <div className='py-2 px-3 border-b mb-4 '>
+                        <span className={`${isDarkTheme?'text-gray-300':'text-black'} text-[15px] font-semibold mb-2`}>Traffic source</span>
                     </div>
-                    <p className="text-gray-400 text-sm">
+                    <p className="text-gray-400 px-3 text-sm">
                         Data will show when video views reach 100
                     </p>
                     <ul className="mt-4 space-y-2 px-4 pb-4">
-                        <li className="flex justify-between text-gray-400 text-sm">
+                        <li className=" text-black text-sm">
+                            <Box sx={{ display: 'flex', justifyContent: 'space-between'}}>
+                                <span>-</span>
+                                <span>{value}%</span>
+                            </Box>
+                            <Box sx={{ width: '100%', position: 'relative', py: 1 }}>
+                                <LinearProgress
+                                 sx={{
+                                    height: 8,
+                                    borderRadius: 4,
+                                    backgroundColor: '#f5f5f5',
+                                    '& .MuiLinearProgress-bar': {
+                                      borderRadius: 4,
+                                      backgroundColor: '#3B82F6',
+                                    },
+                                  }}
+                                variant="determinate" value={value} />
+                            </Box>
+                        </li>
+                        <li className="flex justify-between text-black text-sm">
                             <span>-</span>
                             <span>-%</span>
                         </li>
-                        <li className="flex justify-between text-gray-400 text-sm">
+                        <li className="flex justify-between text-black text-sm">
                             <span>-</span>
                             <span>-%</span>
                         </li>
-                        <li className="flex justify-between text-gray-400 text-sm">
+                        <li className="flex justify-between text-black text-sm">
                             <span>-</span>
                             <span>-%</span>
                         </li>
-                        <li className="flex justify-between text-gray-400 text-sm">
-                            <span>-</span>
-                            <span>-%</span>
-                        </li>
-                        <li className="flex justify-between text-gray-400 text-sm">
+                        <li className="flex justify-between text-black text-sm">
                             <span>-</span>
                             <span>-%</span>
                         </li>
