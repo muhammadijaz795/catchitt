866c0858b4a10df8657a83acbc79303f869af685
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2025-05-08 17:42:44 +0500

    Update helpers.ts

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -344,7 +344,7 @@ export function logPostStats(params: any)
 {
     let allowedTrafficSource = ['for_you', 'following_feed', 'sound_pages', 'profile_pages', 'search_pages', 'direct_messages', 'others'];
     let endpoint = `${process.env.VITE_API_URL}/analytics/v2/watch-time`;
-    let payload =
+    let payload: Record<string, any> =
     {
         "media_id": params.postId,
         "user_id": localStorage.getItem('userId'),
